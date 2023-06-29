4a58e900a9ff87b2c36b84f07a2e84236064862e
AleidvdZ <131220504+AleidvdZ@users.noreply.github.com>
2023-06-29 15:45:15 -0500

    0629 updated work

--- EmployeeSQL/empl_queries.sql
+++ EmployeeSQL/empl_queries.sql
@@ -1,17 +1,17 @@
 -- Data Analysis Pewlett Hackard about people whom the company employed during the 1980s and 1990s.
--- List the employee number, last name, first name, sex, and salary of each employee.
+-- 1. List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 LEFT JOIN salaries s
 	ON e.emp_no = s.emp_no;
 
--- List the first name, last name, and hire date for the employees who were hired in 1986.
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date > '1985-12-31' AND hire_date < '1987-01-01'
 ORDER BY hire_date;
 
--- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+-- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
 FROM dept_manager m
 JOIN departments d
@@ -20,17 +20,45 @@ JOIN employees e
 	ON m.emp_no = e.emp_no
 ORDER BY dept_no;
 
--- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+--4.List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp n
 	ON n.emp_no = e.emp_no
--- 	WHERE e.emp_title_id IS NOT NULL
 JOIN departments d
 	ON d.dept_no = n.dept_no
 ORDER BY emp_no;
 
--- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
--- List each employee in the Sales department, including their employee number, last name, and first name.
--- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
--- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+--5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+ORDER BY last_name;
+
+--6. List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, last_name, first_name
+FROM employees e
+JOIN dept_emp n
+	ON n.emp_no = e.emp_no
+	WHERE n.dept_no 
+	IN (
+		SELECT dept_no
+		FROM departments
+		WHERE dept_name = 'Sales');
+
+--7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, last_name, first_name, d.dept_name
+FROM employees e
+JOIN dept_emp n
+	ON n.emp_no = e.emp_no
+JOIN departments d
+	ON d.dept_no = n.dept_no
+	AND (dept_name = 'Development'
+	OR dept_name = 'Sales'); 
+
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT COUNT(last_name), last_name
+FROM employees
+GROUP BY (last_name)
+ORDER BY COUNT(last_name) DESC;
+
--- EmployeeSQL/empl_table_schemata.sql
+++ EmployeeSQL/empl_table_schemata.sql
@@ -1,39 +1,39 @@
 -- Creating tables for Module 9 Challenge
 -- Create table for titles.csv (1)
 CREATE TABLE titles (
-	title_id VARCHAR NOT NULL, 
-	title VARCHAR NOT NULL,
+	title_id VARCHAR (10) NOT NULL, 
+	title VARCHAR (20) NOT NULL,
 	PRIMARY KEY (title_id)
 );
 
 SELECT * 
-FROM titles
+FROM titles;
 
 -- Create table for departments.csv (2)
 CREATE TABLE departments (
-  	dept_no VARCHAR NOT NULL,
-   	dept_name VARCHAR NOT NULL,
+  	dept_no VARCHAR (5) NOT NULL,
+   	dept_name VARCHAR (20) NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
 SELECT * 
-FROM departments
+FROM departments;
 
 -- Create table for employees.csv (3)
 CREATE TABLE employees (
   	emp_no INTEGER NOT NULL,
 	emp_title_id VARCHAR(10),
 	birth_date DATE,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR,
+	first_name VARCHAR (20) NOT NULL,
+	last_name VARCHAR (20) NOT NULL,
+	sex VARCHAR (1),
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
  );
 
 SELECT * 
-FROM employees
+FROM employees;
 
 -- Create table for salaries.csv (4)
 CREATE TABLE salaries (
@@ -43,24 +43,24 @@ CREATE TABLE salaries (
 );
 
 ALTER TABLE IF EXISTS public.salaries
-    ADD COLUMN salary_id SERIAL PRIMARY KEY
+    ADD COLUMN salary_id SERIAL PRIMARY KEY;
 
 SELECT * 
-FROM salaries
+FROM salaries;
 
 -- Create table for dept_emp.csv (5)
 CREATE TABLE dept_emp (
   	emp_no INTEGER NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR (5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
  );
 
 ALTER TABLE IF EXISTS public.dept_emp
-    ADD COLUMN demp_id SERIAL PRIMARY KEY
+    ADD COLUMN demp_id SERIAL PRIMARY KEY;
 
 SELECT *
-FROM dept_emp
+FROM dept_emp;
 
 -- Create table for dept_manager.csv (6)
 CREATE TABLE dept_manager (
@@ -71,8 +71,8 @@ CREATE TABLE dept_manager (
  );
 
 ALTER TABLE IF EXISTS public.dept_manager
-    ADD COLUMN mng_id SERIAL PRIMARY KEY
+    ADD COLUMN mng_id SERIAL PRIMARY KEY;
 
 SELECT * 
-FROM dept_manager
+FROM dept_manager;
  
